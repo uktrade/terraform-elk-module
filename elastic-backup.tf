3c7f9428ab8985353535791d47500eda1ba2f253
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2018-12-14 16:05:33 +0000

    Default server side encryption for s3 bucket

--- elastic-backup.tf
+++ elastic-backup.tf
@@ -2,7 +2,11 @@ resource "aws_s3_bucket" "elastic-backup" {
   bucket = "${var.service}-elastic-backups"
   acl    = "private"
 
-  tags {
-    Name = "${var.service} elastic backups"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
   }
 }
